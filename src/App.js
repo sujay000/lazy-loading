baa320f2835f75f68f2d1ab56be3bb68b8718efa
sujay000 <sujaysapkal22@gmail.com>
2021-11-18 19:52:05 +0530

    update
    created components and implemented lazy loading for one image

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import React, {Suspense} from "react";
+
+
+const Image = React.lazy(() => import('./Image'));
+
 
 function App() {
+  
   return (
     <div className="App">
      hello world 
+     <Suspense fallback={<div>Loading...</div>} >
+     <Image />
+     </Suspense>
     </div>
   );
 }
---
+++ src/Image.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Image() {
+   return (
+      <div>
+         <img src="https://picsum.photos/400/300" alt="random" />
+      </div>
+   )
+}
