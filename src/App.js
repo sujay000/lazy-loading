b81ea184b9df1c48f12f1737472eb9a107bf7f9f
sujay000 <sujaysapkal22@gmail.com>
2021-11-19 11:11:45 +0530

    change layout
    material ui used for some typography and images layout changes have been done

--- src/App.js
+++ src/App.js
@@ -1,17 +1,46 @@
 import React, { Suspense } from "react";
 import { images } from "./data"; 
-// import Image from "./Image";
+import Typography from '@material-ui/core/Typography';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Grid from '@mui/material/Grid';
 const Image = React.lazy(() => import("./Image"));
+
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      hello world
-      <Suspense fallback={<div>Loading...</div>}>
-        {images.map((image) => (
-          <Image key={image.id} src={image.download_url} />
-        ))}
+
+    <div>
+       <ThemeProvider theme={darkTheme}>
+      <Typography variant="h2" align="center" gutterBottom>
+        Lazy Loading Images
+      </Typography>
+
+      <Suspense 
+        fallback={
+          <div>
+            <Typography variant="h3" align="center" gutterBottom>
+              Loading...
+            </Typography>
+            
+          </div>
+        }>
+        <Grid container spacing={1} > 
+          
+            {images.map((image) => (
+              <Image key={image.id} src={image.download_url} />
+            ))}
+          
+        </Grid>
+        
+
       </Suspense>
+      </ThemeProvider>
     </div>
   );
 }
--- src/Image.jsx
+++ src/Image.jsx
@@ -1,15 +1,19 @@
 import React from "react";
+import Grid from '@mui/material/Grid';
 
 export default function Image(props) {
   return (
-    <div>
+   <Grid item xs={12} sm ={6} md={4} className="gridItem">
+      
       <img
+        className="image"
         src={props.src}
         alt="random"
         loading="lazy"
         width="400px"
         height="300px"
       />
-    </div>
+   
+    </Grid>
   );
 }
